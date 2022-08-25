11d4af00737c6ee6588ef412ff026fae192b4e38
AlexeyOplachko <45398541+AlexeyOplachko@users.noreply.github.com>
2022-08-25 19:10:12 +0300

    Update verifyimport.sh

--- .github/workflows/verifyimport.sh
+++ .github/workflows/verifyimport.sh
@@ -1,5 +1,5 @@
 imports=("AlertService")
-
+echo "TEST"
 for import in ${imports[@]}; do
     formattedImport="import\s*{\s*$import\s*}\s*from\s*'@app\/services"
     grep -i -w -R $formattedImport ./projects ./src
