1f6a9d947e7d73c9340ea0bca9fde59a5cda367e
AlexeyOplachko <45398541+AlexeyOplachko@users.noreply.github.com>
2022-08-28 23:57:47 +0300

    Delete verifyimport.sh

--- .github/workflows/verifyimport.sh
+++
@@ -1,7 +0,0 @@
-imports=("AlertService")
-echo "TEST"
-for import in ${imports[@]}; do
-    formattedImport="import\s*{\s*$import\s*}\s*from\s*'@app\/services"
-    grep -i -w -R $formattedImport ./projects ./src
-done
-exit 0
